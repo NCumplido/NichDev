2cf50bdf91fb9d2d65f8d02fee5761a8f0084a4a
Nachodood <ncumplido@msn.com>
2021-01-02 13:41:57 +0000

    Complete part 4

---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PostDate { get; set; }
+        public string Topic { get; set; }
+        public string Content{ get; set; }
+        public int Votes { get; set; }
+    }
+}
